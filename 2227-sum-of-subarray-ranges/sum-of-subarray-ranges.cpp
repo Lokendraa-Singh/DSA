e3da03729301f68714cd79f2f09cfe3f84aae709
Lokendraa-Singh <lokendrasingh15732@gmail.com>
2026-08-08 17:03:34 +0530

    Time: 7 ms (40.20%) | Memory: 16.1 MB (9.68%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,21 +1,141 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        
-        long long totalSum=0;
-        for(int i=0;i<nums.size();i++){
-            int mini=nums[i];
-            int maxi=nums[i];
 
-            for(int j=i;j<nums.size();j++){
-                mini=min(mini,nums[j]);
-                maxi=max(maxi,nums[j]);
+    // Next Smaller Element
+    vector<int> getNSE(vector<int>& arr, int n) {
 
-                long long diff=(maxi-mini);
-                totalSum=totalSum+diff;
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
             }
+
+            ans[i] = !st.empty() ? st.top() : n;
+
+            st.push(i);
         }
 
-        return totalSum;
+        return ans;
+    }
+
+
+    // Previous Smaller Element
+    vector<int> getPSE(vector<int>& arr, int n) {
+
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = !st.empty() ? st.top() : -1;
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+
+    // Sum of all subarray minimums
+    long long sumOfSubArrMin(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int> PSE = getPSE(arr, n);
+        vector<int> NSE = getNSE(arr, n);
+
+        long long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            long long ls = i - PSE[i];
+            long long rs = NSE[i] - i;
+
+            long long totalWays = ls * rs;
+
+            sum += totalWays * arr[i];
+        }
+
+        return sum;
+    }
+
+
+    // Next Greater Element
+    vector<int> getNGE(vector<int>& arr, int n) {
+
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && arr[st.top()] <= arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = !st.empty() ? st.top() : n;
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+
+    // Previous Greater Element
+    vector<int> getPGE(vector<int>& arr, int n) {
+
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && arr[st.top()] < arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = !st.empty() ? st.top() : -1;
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+
+    // Sum of all subarray maximums
+    long long sumOfSubArrMax(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int> PGE = getPGE(arr, n);
+        vector<int> NGE = getNGE(arr, n);
+
+        long long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            long long ls = i - PGE[i];
+            long long rs = NGE[i] - i;
+
+            long long totalWays = ls * rs;
+
+            sum += totalWays * arr[i];
+        }
+
+        return sum;
+    }
+
+
+    // Final answer
+    long long subArrayRanges(vector<int>& nums) {
+
+        return sumOfSubArrMax(nums) - sumOfSubArrMin(nums);
     }
 };
