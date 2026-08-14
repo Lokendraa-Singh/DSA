27fbacfe179c6a7ff1ab57038a4a306588d41b8e
Lokendraa-Singh <lokendrasingh15732@gmail.com>
2026-08-14 14:09:34 +0530

    Time: 2 ms (51.93%) | Memory: 9.5 MB (57.72%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-        int ans=INT_MIN;
-        for (int i = 0; i < s.size(); i++) {
-            
-            for (int j = i; j < s.size(); j++) {
-                string temp = "";
-                temp = temp + s.substr(i,j - i + 1);
-                unordered_map<char, int> mpp;
-                int length =0;
-            
-                for (int k = 0; k < temp.size(); k++) {
-                    mpp[temp[k]]++;
-                }
-                bool valid=true;
-                for (auto x : mpp) {
-                    if (x.second > 2) {
-                        valid = false;
-                        break;
-                    }
-                }
+        int n = s.size();
+        int j = 0;
+        int ans = 0;
+        unordered_map<char, int> mpp;
 
-                if (valid) {
-                    ans = max(ans, (int)temp.size());
+        for (int i = 0; i < s.size(); i++) {
+            mpp[s[i]]++;
+
+            if (mpp[s[i]] > 2) {
+                while (mpp[s[i]] > 2) {
+                    mpp[s[j]]--;
+                    j++;
                 }
             }
+            ans = max(ans, i - j + 1);
         }
 
         return ans;
