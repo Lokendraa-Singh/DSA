1a2af23c200008b077341ff148a8dfe597ebf3a2
Lokendraa-Singh <lokendrasingh15732@gmail.com>
2026-09-03 12:49:44 +0530

    Time: 13 ms (38.74%) | Memory: 178 MB (9.95%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+
+        int n = nums1.size();
+        vector<int> nums2(n);
+
+        stack<int> st;
+
+        // all even check
+        bool temp = true;
+        for (int i = 0; i < nums1.size(); i++) {
+
+            if (nums1[i] % 2 != 0) {
+                temp = false;
+                break;
+            }
+        }
+        if (temp) {
+            return true;
+        }
+
+        // all odd check
+        temp = true;
+        for (int i = 0; i < nums1.size(); i++) {
+
+            if (nums1[i] % 2 == 0) {
+                temp = false;
+                break;
+            }
+        }
+
+        if (temp) {
+            return true;
+        }
+
+        for (int i = 0; i < nums1.size(); i++) {
+            if (nums1[i] % 2 != 0) {
+                st.push(nums1[i]);
+            }
+        }
+
+        for (int i = 0; i < nums1.size(); i++) {
+
+            if (nums1[i] % 2 == 0) {
+                temp = false;
+                while (!st.empty() && temp == false) {
+                    if (nums1[i] - st.top() >= 1) {
+                        nums2[i] = nums1[i] - st.top();
+                        temp = true;
+                    } else {
+                        st.pop();
+                    }
+                }
+            } else {
+                nums2[i] = nums1[i];
+            }
+        }
+        temp = true;
+        for (int i = 0; i < nums2.size(); i++) {
+            if (nums2[i] % 2 == 0) {
+                temp = false;
+                break;
+            }
+        }
+
+        return temp;
+    }
+};
