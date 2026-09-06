35492142212594f85a778c4ac060e6a06505d369
Lokendraa-Singh <lokendrasingh15732@gmail.com>
2026-09-06 13:57:38 +0530

    Time: 860 ms (19.31%) | Memory: 347.2 MB (5.24%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        
-         vector<pair<float, vector<int>>> v;
+
+        vector<pair<float, vector<int>>> v;
         for (int i = 0; i < arr.size(); i++) {
             for (int j = i + 1; j < arr.size(); j++) {
-                float fraction= (float)arr[i]/arr[j];
+                float fraction = (float)arr[i] / arr[j];
 
-                v.push_back({fraction,{arr[i],arr[j]}});
+                v.push_back({fraction, {arr[i], arr[j]}});
             }
         }
 
         sort(v.begin(), v.end(),
-             [](auto& a, auto& b) { return a.first < b.first; 
-             });
+             [](auto& a, auto& b) { return a.first < b.first; });
 
-        
-        return v[k-1].second;
+        return v[k - 1].second;
     }
 };
