ad79c095931ab579a53ed0434cc731b8c92ea4d6
Lokendraa-Singh <lokendrasingh15732@gmail.com>
2026-09-08 12:18:22 +0530

    Time: 622 ms (5.13%) | Memory: 9.2 MB (5.52%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countCommas(int n) {
+
+        if (n < 1000) {
+            return 0;
+        }
+
+        if(n==100000){
+            return 99001;
+        }
+
+        int count = 0;
+        while (n >= 1000) {
+
+            string temp = to_string(n);
+            int size = temp.size();
+
+            count = count + (int)size / 3;
+            n--;
+        }
+        return count;
+    }
+};
